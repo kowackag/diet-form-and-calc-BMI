7f33a1aeedf5ee63a15b08db135a586e95452715
mkowack <kowackag@gmail.com>
2024-01-13 23:50:04 +0100

    lastStageValidationSchema - message

--- src/components/LastStage/lastStageValidationSchema.ts
+++ src/components/LastStage/lastStageValidationSchema.ts
@@ -5,15 +5,15 @@ export const lastStageValidateSchema = yup.object().shape({
   personalData: yup.object().shape({
     userName: yup
       .string()
-      .required("Podaj płeć")
-      .min(3, "Wpisz poprawnie imię i Nazwisko"),
+      .required("Podaj swoje imię i nazwisko")
+      .min(3, "Wpisz poprawnie imię i nazwisko"),
     userEmail: yup
       .string()
       .required("Podaj adres e-mail")
       .email("Nieprawidłowy format adresu e-mail"),
     userPhone: yup
       .string()
-      .required("Podaj wzrost")
+      .required("Podaj numer telefonu")
       .matches(regPhone, "Wprowadzono błędny numer telefonu"),
     userInfo: yup.string(),
   }),
